b9d0cf903652009674257c372518afe9bd13def3
kota oue <kotaoue@gmail.com>
2022-01-14 22:51:50 +0900

    copy example from https://pkg.go.dev/google.golang.org/api@v0.65.0/fitness/v1

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"google.golang.org/api/fitness/v1"
+)
 
 func main() {
 	fmt.Println("go-fits")
+
+	ctx := context.Background()
+	fitnessService, err := fitness.NewService(ctx)
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+	fmt.Println(fitnessService)
 }
